715d2e94a0c77cc8b76c9259fec80b4ca42dbbde
Serhii Stepanchuk <51526907+serjukst@users.noreply.github.com>
2019-10-08 15:25:46 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.match(/.{1,10}/g);
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] === '**********') {
+            arr[i] = ' ';
+
+        }    
+        else {
+            arr[i] = arr[i].match(/.{1,2}/g);
+            let newArr = arr[i];
+            for (let j = 0; j < newArr.length; j++) {
+
+                if (newArr[j] === '00') {
+                   newArr[j] = '';
+                } else if (newArr[j] === '10') {
+                    newArr[j] = '.';
+                } else {
+                     newArr[j] = '-';
+                }
+                
+            }
+
+            arr[i] = newArr.join('')
+        }
+
+    }
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+
+        if (MORSE_TABLE[arr[i]] === undefined) {
+            MORSE_TABLE[arr[i]] = " ";
+        }
+
+      result.push(MORSE_TABLE[arr[i]]);
+
+    }
+    return result.join('');
+
 }
 
 module.exports = {
